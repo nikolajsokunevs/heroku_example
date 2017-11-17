20f7083d402ef61d31170de8afba2e39a23558b6
Nikolay Okunev <nikolay.okunev@ctco.lv>
2017-11-17 08:29:22 +0200

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,12 +8,12 @@ import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        
-        final String baseUri = "http://localhost:"+(System.getenv("PORT")!=null?System.getenv("PORT"):"9998")+"/test/";
+
+        final String baseUri = "http://localhost:"+(System.getenv("PORT")!=null?System.getenv("PORT"):"8080")+"/test/";
         final Map<String, String> initParams = new HashMap<String, String>();
 
         initParams.put("com.sun.jersey.config.property.packages","resources");
-        
+
         System.out.println("Starting grizzly...");
         SelectorThread threadSelector = GrizzlyWebContainerFactory.create(baseUri, initParams);
         System.out.println(String.format("Jersey started with WADL available at %sapplication.wadl.",baseUri, baseUri));
