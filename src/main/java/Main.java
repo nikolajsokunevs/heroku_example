afa8eca2cc7ac12b3e664f4783fbc8544b1ebdae
Nikolay Okunev <nikolay.okunev@ctco.lv>
2017-11-17 08:26:39 +0200

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
 public class Main {
     public static void main(String[] args) throws IOException {
         
-        final String baseUri = "http://localhost:"+(System.getenv("PORT")!=null?System.getenv("PORT"):"9998")+"/";
+        final String baseUri = "http://localhost:"+(System.getenv("PORT")!=null?System.getenv("PORT"):"9998")+"/test/";
         final Map<String, String> initParams = new HashMap<String, String>();
 
         initParams.put("com.sun.jersey.config.property.packages","resources");
